1d2d439004f6f67fd1b3552d6f39a96fbab1b3eb
Gurpartap <gurpartap6125@gmail.com>
2023-05-27 17:37:55 +0530

    initail note book app

---
+++ script.js
@@ -0,0 +1,48 @@
+const addBtn=document.getElementById('add')
+
+addBtn.addEventListener('click',()=>{
+    addNewNote();
+})
+
+function addNewNote(){
+    const note=document.createElement("div")
+    note.classList.add("note")
+    note.innerHTML=`<div class="notes">
+    <div class="tools">
+        <button class="edit"><i class="fa-solid fa-pen-to-square"></i></button>
+        <button class="delete"><i class="fa-solid fa-trash"></i></button>
+    </div>
+    <div class="main hidden"></div>
+    <textarea ></textarea>`;
+
+    const editBtn=note.querySelector(".edit")
+const deleteBtn=note.querySelector(".delete")
+
+const main=note.querySelector(".main")
+const textArea=note.querySelector("textarea")
+
+editBtn.addEventListener('click',()=>{
+
+    main.classList.toggle("hidden")
+    textArea.classList.toggle("hidden")
+}
+)
+
+deleteBtn.addEventListener('click',()=>{
+    note.remove();
+})
+
+textArea.addEventListener('input',(e)=>{
+    const {value}=e.target
+    console.log(value)
+
+    main.innerHTML=marked.parse(value)
+})
+
+
+
+    document.body.appendChild(note)
+}
+
+
+
